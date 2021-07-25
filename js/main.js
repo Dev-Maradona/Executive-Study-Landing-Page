102a80e103181d20f99c462f467bc9098ae790e1
Maradona <70854249+Dev-Maradona@users.noreply.github.com>
2021-07-25 19:20:12 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     
     // Active Links in navBar
 
-    $('.links li').on('click', function() {
+    $('.main-nav .links li').on('click', function() {
+        // if ($('.main-nav .links li').is(':last-child')) {
+        //     $(this).addClass('').siblings().removeClass('active');
+        // }
         $(this).addClass('active').siblings().removeClass('active');
     });
 
@@ -10,9 +13,34 @@ $(document).ready(function(){
     $('.theCard').on('click', function() {
         $(this).addClass('active');
         $(this).siblings().removeClass('active');
+    });
+
+    $('#open-collapse').on('click', function() {
+
+        $(this).hide(1000)
+        $('.sm-nav').show(1000)
+    });
+
+    $('#close-collapse').on('click', function() {
+        $('#open-collapse').show(1000)
+        $('.sm-nav').hide(1000)
+    });
+
+    var btnToUp = $('.to-up')
+
+    $(window).on('scroll', function() {
+        if (document.documentElement.scrollTop > 500 || document.body.scrollTop > 500) {
+            btnToUp.show()
+        } else {
+            btnToUp.hide()
+        }
+    });
+
+    btnToUp.on('click', function() {
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
     })
 
     // Code To Trigger Bx Slider
     $('.slider').bxSlider();
 });
-
